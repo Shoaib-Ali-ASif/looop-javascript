a0ebf1e1c17df971132990d84501de90e234923b
shoaib-ali-asif <eagleshebi@gmail.com>
2023-06-04 22:01:38 +0500

    Update app.js

---
+++ javascript/app.js
@@ -0,0 +1,28 @@
+// Sum all the array elements by using a loop.
+
+
+
+// Reverse the array by using a loop.
+
+
+
+// Make a table of the given number with all possible loops.
+
+
+
+// Find the largest number in an array.
+
+
+
+
+// Find the smallest number in an array.
+
+
+
+
+// Make an array to store the name of 5 students and iterate with for and foreach loop.
+
+
+
+
+// Make an object to store the information of a student and iterate with a for-in loop.
