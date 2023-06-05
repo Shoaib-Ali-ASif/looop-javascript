31bae98da93a096c7a89bd1357c95ca2d6422a06
shoaib-ali-asif <eagleshebi@gmail.com>
2023-06-05 11:54:06 +0500

    Update app.js

--- javascript/app.js
+++ javascript/app.js
@@ -1,41 +1,35 @@
 // Sum all the array elements by using a loop.
-
-
+let Arr = [1,2,3,4];
+let sum = 0;
 
 
 
 // Reverse the array by using a loop.
 
-
-
 // Make a table of the given number with all possible loops.
 
-
+let number = 2;
+for ( let i = 1; i <= 10; i++){
+    result = i * number;
+    console.log(`${number} * ${i} = ${result} `);
+}
 
 // Find the largest number in an array.
 
-
+let num = [ 1 , 3 , 6, 9 , 15];
 
 
 // Find the smallest number in an array.
 
-
-
-
 // Make an array to store the name of 5 students and iterate with for and foreach loop.
-let students = ["Ali", "Ahmad", "Hassan", "Kaleem" , "farhan"];
-for(let i = 0; i < students.length; i++) 
-{
-    console.log(students[i]); 
+let students = ["Ali", "Ahmad", "Hassan", "Kaleem", "farhan"];
+for (let i = 0; i < students.length; i++) {
+  console.log(students[i]);
 }
-    
-
-
 
 // Make an object to store the information of a student and iterate with a for-in loop.
 
-
-let studentInfo= ["Ali", "Ahmad", "Hassan", "Kaleem" , "farhan"];
- for(let element in studentInfo) { 
-        console.log(studentInfo[element]); 
-    }
+let studentInfo = ["Ali", "Ahmad", "Hassan", "Kaleem", "farhan"];
+for (let element in studentInfo) {
+  console.log(studentInfo[element]);
+}
